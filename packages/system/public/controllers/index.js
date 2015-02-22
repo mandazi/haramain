f7667c95027d7bab5042eaf481bd16785928e395
Hayat Noor <hnoor@bbg.gov>
2015-02-21 19:12:51 -0500

    added search

--- packages/system/public/controllers/index.js
+++ packages/system/public/controllers/index.js
@@ -57,6 +57,19 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
       }
     });
 */
+
+	  $scope.friends = [
+		  { name: "Peter",   age: 20 },
+		  { name: "Pablo",   age: 55 },
+		  { name: "Linda",   age: 20 },
+		  { name: "Marta",   age: 37 },
+		  { name: "Othello", age: 20 },
+		  { name: "Markus",  age: 32 }
+	  ];
+
+	  $scope.filterFunction = function(element) {
+		  return element.name.match(/^Ma/) ? true : false;
+	  };
 	$scope.loadAll = function () {
 		GetHaramainAudioData.then(function (response) {
 			$rootScope.audioTracks = response.data;
